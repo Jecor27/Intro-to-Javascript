85496e6e4db2f7d86152356b43de94c1acd5312b
Jesus Corrales <jjcorrales76@gmail.com>
2026-04-14 23:10:20 -0600

    added import/export with function

---
+++ hello.js
@@ -0,0 +1,8 @@
+import { personalInfo } from "./personalInfo.js";
+
+//lab 1
+console.log("Hello, JavaScript! I am Jesus.");
+
+
+//lab 2
+personalInfo();
---
+++ personalInfo.js
@@ -0,0 +1,11 @@
+function personalInfo(){
+const name = "Jesus Corrales"
+const age = 29  
+const height = 72
+const isStudent = true
+
+console.log(`Hello, my name is ${name}. I am ${age} years old, ${height} inches tall, and it is ${isStudent} that I am a learner.`)
+}
+
+
+export {personalInfo};
