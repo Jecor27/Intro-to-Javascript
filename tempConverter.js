44c1c8f2e26b3d6d1923fbcdc534f82ad0adfc57
Jesus Corrales <jjcorrales76@gmail.com>
2026-04-15 00:08:27 -0600

    added a while loop

--- tempConverter.js
+++ tempConverter.js
@@ -1,11 +1,15 @@
 function tempConverter() {
-  let celsiusInput = prompt("Enter temperature in Celsius:");
-  let celsius = Number(celsiusInput);
-  if (!celsius) {
-    alert("Invalid entry");
-  } else {
-    let fahrenheit = (celsius * 9) / 5 + 32;
-    alert(`${celsius}°C is equivalent to ${fahrenheit}°F`);
+  let promptAgain = "yes";
+  while (promptAgain == "yes") {
+    let celsiusInput = prompt("Enter temperature in Celsius:");
+    let celsius = Number(celsiusInput);
+    if (!celsius) {
+      alert("Invalid entry");
+    } else {
+      let fahrenheit = (celsius * 9) / 5 + 32;
+      alert(`${celsius}°C is equivalent to ${fahrenheit}°F`);
+    }
+    promptAgain = prompt("Want another conversion? (yes/no)");
   }
 }
 
